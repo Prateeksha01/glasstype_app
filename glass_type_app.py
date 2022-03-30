895bcbc6c40a208a76e47c3f517814b77ff04451
Prateeksha01 <86128495+Prateeksha01@users.noreply.github.com>
2022-03-30 13:34:12 +0530

    Update glass_type_app.py

--- glass_type_app.py
+++ glass_type_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
 from sklearn.metrics import precision_score, recall_score 
 
